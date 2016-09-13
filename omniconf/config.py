c6ffaaeb17434d1d99853f18020748868d6641e5
Nick Douma <n.douma@nekoconeko.nl>
2016-09-13 16:50:03 +0200

    First implementation of config.py

--- omniconf/config.py
+++ omniconf/config.py
@@ -17,6 +17,47 @@
 # License along with this library. If not, see
 # <http://www.gnu.org/licenses/>.
 
+from .exceptions import UnknownSettingError, UnconfiguredSettingError
+from .settings import DEFAULT_REGISTRY as SETTINGS_REGISTRY
 
-def config():
-    pass
+
+class ConfigRegistry(object):
+    def __init__(self, settings_registry=None):
+        global SETTINGS_REGISTRY
+        if not settings_registry:
+            self.settings = SETTINGS_REGISTRY
+        self.registry = {}
+
+    def set(self, key, value):
+        if not self.settings.has(key):
+            raise UnknownSettingError("Trying to configure unregistered key {0}".format(key))
+        setting = self.settings.get(key)
+        self.settings[key] = setting.type(value)
+
+    def has(self, key):
+        if key in self.registry:
+            return True
+        elif key in self.settings and self.settings[key].default is not None:
+            return True
+        return False
+
+    def get(self, key):
+        if key in self.registry:
+            return self.registry[key]
+        elif key in self.settings and self.settings[key].default is not None:
+            return self.settings[key].default
+        raise UnconfiguredSettingError("No value or default available for {0}".format(key))
+
+    def unset(self, key):
+        if key in self.registry:
+            del self.registry[key]
+
+DEFAULT_REGISTRY = ConfigRegistry()
+
+
+def config(key, registry=None):
+    global DEFAULT_REGISTRY
+    if not registry:
+        registry = DEFAULT_REGISTRY
+
+    return registry.get(key)
