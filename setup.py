8d655c30765ffce2418dbe3e91e35e7261368dad
Nick Douma <n.douma@nekoconeko.nl>
2017-04-26 17:04:56 +0200

    Move metadata and requirements to setup.cfg, and enable pbr

--- setup.py
+++ setup.py
@@ -19,66 +19,9 @@
 # <http://www.gnu.org/licenses/>.
 
 
-from setuptools import setup, find_packages
-
-DESCRIPTION = "A Python library that makes configuring your application "\
-              "independent from your configuration backend."
-LONG_DESCRIPTION = open('README.rst').read()
-NAME = "omniconf"
-VERSION = "1.1.1"
-BUILD = "1064891"
-
+from setuptools import setup
 
 setup(
-    name=NAME,
-    version=VERSION,
-    description=DESCRIPTION,
-    long_description=LONG_DESCRIPTION,
-    license="LGPL3",
-    author="Nick Douma",
-    author_email="n.douma@nekoconeko.nl",
-    url="https://github.com/cyso/omniconf",
-    packages=find_packages(),
-    data_files=[('', ['README.rst'])],
-    install_requires=[],
-    extras_require={
-        "configobj": [
-            "configobj"
-        ],
-        "vault": [
-            "hvac"
-        ],
-        "yaml": [
-            "PyYAML"
-        ]
-    },
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Information Technology",
-        "Intended Audience :: System Administrators",
-        "License :: OSI Approved :: "
-            "GNU Lesser General Public License v3 or later (LGPLv3+)",
-        "Operating System :: POSIX",
-        "Operating System :: POSIX :: Linux",
-        "Operating System :: Unix",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: "
-            "Implementation :: CPython",
-        "Programming Language :: Python :: "
-            "Implementation :: Jython",
-        "Programming Language :: Python :: "
-            "Implementation :: PyPy",
-        "Topic :: Software Development :: "
-            "Libraries :: Python Modules",
-        "Topic :: Software Development :: User Interfaces",
-        "Topic :: System :: Installation/Setup",
-        "Topic :: Utilities"
-    ]
+    setup_requires=['pbr>=1.9', 'setuptools>=17.1'],
+    pbr=True,
 )
